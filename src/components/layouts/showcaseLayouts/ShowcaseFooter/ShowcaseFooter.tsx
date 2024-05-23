4baa95f737a9cc028cc5c1f89e3aca5b65ddef77
Taalaibekovi4 <148772481+Taalaibekovi4@users.noreply.github.com>
2024-05-23 21:36:12 +0600

    MARKET
    OSH

---
+++ src/components/layouts/showcaseLayouts/ShowcaseFooter/ShowcaseFooter.tsx
@@ -0,0 +1,7 @@
+import classes from './ShowcaseFooter.module.css';
+
+const ShowcaseFooter: React.FC = () => {
+  return <footer className={classes.footer}> © 2024 все права зашишены </footer>;
+};
+
+export default ShowcaseFooter;
